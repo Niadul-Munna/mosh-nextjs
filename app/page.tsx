20b40be474c09f06095ec25118cbcbea4c0eaeec
niadulMunna <niadulislammunna@gmail.com>
2025-03-25 23:06:44 +0600

    style: added tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex gap-10 bg-emerald-300 text-black p-3">
       <h1>Home</h1>
-      <Link href="/user">Users</Link>
+      <Link className="hover:underline" href="/user">
+        Users
+      </Link>
+      <div>
+        <Link className="hover:underline" href="/about">
+          Login
+        </Link>
+      </div>
     </div>
   );
 }
